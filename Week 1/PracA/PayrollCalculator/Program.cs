839c5bf70fc2156f64e672016307e9493c3a3dcb
Aydin891 <zamay012@mymail.unisa.edu.au>
2026-08-06 16:45:05 +0930

    feat: translate payroll to C#

---
+++ Week 1/PracA/PayrollCalculator/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MyApp
+{
+    static class Program
+    {
+        const double TaxRate = 0.2;
+        static double calculate_pay(double hours, double rate)
+        {
+            if(hours < 0 || rate < 0 )
+            {
+                throw new ArgumentException("Hours and rate must be positive.");
+            }
+            
+        double gross = hours * rate;
+        double tax = gross * TaxRate;
+        double net = gross - tax;
+        
+        return net;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter employee name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Hours worked: ");
+            double hours = double.Parse(Console.ReadLine());
+            
+            Console.Write("Hourly rate: ");
+            double rate =  double.Parse(Console.ReadLine());
+
+            double net_pay = calculate_pay(hours, rate);
+
+            Console.WriteLine($"{name} earned ${net_pay:F2} after tax. ");
+
+        }
+    }
+}
+
+        
+
---
+++ Week 1/PracA/Person/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MyApp
+{
+    static class Program
+    {
+        public class Person
+        {
+            public string firstName;
+            public string lastName;
+            public int Age;
+
+            public Person(string first, string last, int age)
+            {
+                firstName = first;
+                lastName = last;
+                Age = age;
+            }
+
+            public string FullName()
+            {
+                return $"{firstName} {lastName}";
+            }
+
+            public bool IsAdult()
+            {
+                return Age >= 18;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("John", "Marston", 45);
+
+            Console.WriteLine(person1.FullName());
+            Console.WriteLine(person1.IsAdult());
+        }
+    }
+}
