220e12073e5e487eb2f2ed4d78cca99495c0ea69
Aydin891 <zamay012@mymail.unisa.edu.au>
2026-08-10 13:11:47 +0930

    Feat: PracB

---
+++ Week 1/PracB/ToDo/TodoApp.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var tasks = new List<string> {};
+
+            Console.WriteLine("==========To Do==========");
+
+            Console.WriteLine("Add to add tasks");
+            Console.WriteLine("Show to see your tasks");
+            Console.WriteLine("Remove to remove tasks");
+            Console.WriteLine("Clear to clear all tasks");
+            Console.WriteLine();
+
+
+
+            while (true)
+            {
+                Console.Write("What you wnat to do?");
+                string userInput = Console.ReadLine();
+                switch(userInput)
+                {
+                    case "Add":
+                        {
+                        Console.Write("Please enter your task: ");
+                        string taskInput = Console.ReadLine();
+                        tasks.Add(taskInput);
+                        Console.WriteLine("Your task has been added.");
+                        break;   
+                        }
+
+                    
+                    case "Show":
+                        {
+                        Console.WriteLine("Your taks:");
+                        foreach (string task in tasks)
+                        {
+                            Console.WriteLine(task);
+                        }
+                        break;
+                        
+                        }
+
+                    case "Remove":
+                        {
+                        Console.Write("Please enter your task to remove: ");
+                        string taskInput = Console.ReadLine();
+                        tasks.Remove(taskInput);
+                        Console.WriteLine("Your task has been removed.");
+                        break;
+                        }
+
+
+                    case "Clear":
+                        {
+                        tasks.Clear();
+                        Console.WriteLine("Your tasks has been cleared.");
+                        break;
+                        }
+
+
+                    default:
+                        Console.WriteLine("Input not valid.");
+                        break;
+                    
+
+
+
+
+ 
+                       
+                }   
+
+            }
+
+
+        }
+    }
+}
