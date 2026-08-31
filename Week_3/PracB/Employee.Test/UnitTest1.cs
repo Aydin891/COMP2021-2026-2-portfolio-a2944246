3551fc80258b8d689daa8cdb080e19bd7ff08a35
Aydin891 <zamay012@mymail.unisa.edu.au>
2026-08-31 14:26:00 +0930

    Topic 3 Prac B Task 3 UnitTest

--- Week_3/PracB/Employee.Test/UnitTest1.cs
+++ Week_3/PracB/Employee.Test/UnitTest1.cs
@@ -5,6 +5,17 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
+        FullTimeEmployee employee1 = new FullTimeEmployee{Name = "Arthur", AnnualSalary = 48000m};
 
+        decimal result = employee1.CalculatePay();
+        Assert.Equal(38400m, result);
+    }
+    [Fact]
+    public void Test2()
+    {
+        Contractor contractor1 = new Contractor{Name ="John", Rate = 50m, Hours = 20m};
+
+        decimal result = contractor1.CalculatePay();
+        Assert.Equal(800m, result);
     }
 }
