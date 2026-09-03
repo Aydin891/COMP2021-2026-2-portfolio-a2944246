37f5e899a1680ca73d93f89918ac72e6420124f7
Aydin891 <zamay012@mymail.unisa.edu.au>
2026-09-03 16:56:38 +0930

    Week 5: Prac A  Task 2

---
+++ Week_5/Lists/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>("Arthur", "John", "Max", "Allen", "Bruce");
+
+            names.Add("Sarah");
+            names.Add("Olivia");
+            names.Remove("Olivia");
+            names.Insert(1, "Jack");
+            names.AddRange(new string []{"Emma", "Daniel", "Mia"});
+
+            int index = names.IndexOf("Arthur");
+            Console.WriteLine($"Arthur index is: {index}");
+
+        }
+    }
+}
